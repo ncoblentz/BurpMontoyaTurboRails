5a8b4814e81991e524f8ccd8a08115da8af7d1de
Nick Coblentz <nick.coblentz@gmail.com>
2023-08-03 13:50:03 -0500

    Fix Publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,8 +51,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/ncoblentz/burpmontoyaturborails")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GHUSERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GHTOKEN")
             }
         }
     }
